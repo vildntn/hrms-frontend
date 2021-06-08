b39ec9df536bb75a802e762d0d29408955669292
Lenovo <Lenovo@DESKTOP-TCNT505>
2021-06-08 22:02:45 +0300

    first commit

--- src/layouts/Detail.jsx
+++ src/layouts/Detail.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import CandiateList from "../pages/CandiateList";
-import EmployerList from '../pages/EmployerList'
-import StaffList from '../pages/StaffList'
+import EmployerList from "../pages/EmployerList";
+import StaffList from "../pages/StaffList";
 export default function Section() {
   return (
     <div>
       <Grid>
         <Grid.Row>
-            <Grid.Column width={12}>
+          <Grid.Column width={12}>
             <EmployerList />
-            </Grid.Column>
-         
+          </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-            <Grid.Column width={12}>
-            <CandiateList/>
-            </Grid.Column>
-         
+          <Grid.Column width={12}>
+            <CandiateList />
+          </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-            <Grid.Column width={12}>
+          <Grid.Column width={12}>
             <StaffList />
-            </Grid.Column>
-         
+          </Grid.Column>
         </Grid.Row>
-    
       </Grid>
     </div>
   );
--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -1,22 +1,13 @@
-import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import React from "react";
+import { Menu } from "semantic-ui-react";
 export default function Categories() {
-    return (
-        <div>
-              <Menu inverted icon="labeled" vertical>
-        <Menu.Item
-          name='Staff List'
-
-        />
-        <Menu.Item
-          name='Employer List '
-
-        />
-        <Menu.Item
-          name='Candidate List'
-
-        />
+  return (
+    <div>
+      <Menu inverted icon="labeled" vertical>
+        <Menu.Item name="Staff List" />
+        <Menu.Item name="Employer List " />
+        <Menu.Item name="Candidate List" />
       </Menu>
-        </div>
-    )
+    </div>
+  );
 }
