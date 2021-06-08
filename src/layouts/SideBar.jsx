e1c40c8b1b20456d27c4d3a83d886ca921c38ce8
Lenovo <Lenovo@DESKTOP-TCNT505>
2021-06-08 22:03:52 +0300

    first commit

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -3,11 +3,17 @@ import { Menu } from "semantic-ui-react";
 export default function Categories() {
   return (
     <div>
+
       <Menu inverted icon="labeled" vertical>
+        
         <Menu.Item name="Staff List" />
+
         <Menu.Item name="Employer List " />
+
         <Menu.Item name="Candidate List" />
+
       </Menu>
+
     </div>
   );
 }
