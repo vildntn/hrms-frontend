54c61cc2bca2370d7a4585c9fd326a0aac227483
vildntn <vildant35@gmail.com>
2021-12-30 14:12:32 +0300

    signup page modified

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import { Form, Formik } from "formik";
-import { Button, Header, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Header,
+  Segment,
+  Grid,
+  Image,
+  Message,
+  FormInput,
+} from "semantic-ui-react";
 import * as Yup from "yup";
-import HrmsTextInput from "../../utilities/customFormControls/HrmsTextInput";
+// import HrmsTextInput from "../../utilities/customFormControls/HrmsTextInput";
 
 export default function SignUp() {
+  const [people, setPeople] = useState([])
+  const [person, setPerson] = useState({
+  email: "",
+  firstName: "",
+  lastName: "",
+  password: "",})
   const initialValues = {
     email: "",
     firstName: "",
@@ -18,49 +32,88 @@ export default function SignUp() {
     password: Yup.string().required(),
   });
 
+  const handleSubmit = (e) => {
+     e.preventDefault();
+     const newPerson={...person,id:new Date().getTime().toString()}
+     setPeople([...people,newPerson])
+     console.log(newPerson)
+
+  };
+
+const handleChange=(e)=>{
+  const name=e.target.name;
+  const value=e.target.value;
+ setPerson({...person,[name]:value})
+}
+
+
+
   return (
-    <div>
-      <div className="ui placeholder segment">
-        <Formik
-          initialValues={initialValues}
-          validationSchema={shema}
-          onSubmit={(values) => {
-            console.log(values);
-          }}
-        >
-          <Form className="ui form">
-            <div className="ui header">
-              <Header>Create Account</Header>
-            </div>
-            <Segment>
-            <label>First Name</label>
-            <HrmsTextInput
-              name="firstName"
-              placeholder="First Name"
-            ></HrmsTextInput>
-            <label>Last Name</label>
-            <HrmsTextInput
-              name="lastName"
-              placeholder="Last Name"
-            ></HrmsTextInput>
-            <label>Email</label>
-            <HrmsTextInput
-              name="email"
-              placeholder="email@email.com"
-            ></HrmsTextInput>
-            <label>Password</label>
-            <HrmsTextInput
-              name="password"
-              placeholder="Password"
-            ></HrmsTextInput>
-            <Button className="ui orange fluid button" color="black">
-              Register
-            </Button>
-            </Segment>
-           
-          </Form>
-        </Formik>
-      </div>
-    </div>
+    <>
+      <Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
+      >
+        <div className="column" style={{ maxWidth: 450 }}>
+          <Header as="h2" color="teal" textAlign="center">
+            <Image src="/logo.png" /> Log-in to your account
+          </Header>
+          <Formik  initialValues={person}>
+            <Form size="large"  className="ui form">
+              <Segment stacked>
+                <FormInput
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="E-mail address"
+                  name="email"
+                 
+                  onChange={handleChange}
+                />
+                <FormInput
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="First Name"
+                  name="firstName"
+                  value={person.firstName}
+                  onChange={handleChange}
+                />
+                <FormInput
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Last Name"
+                  name="lastName"
+                  value={person.lastName}
+                  onChange={handleChange}
+                />
+
+                <FormInput
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  value={person.password}
+                  onChange={handleChange}
+                />
+
+                <Button color="teal" fluid size="large" type="submit" onClick={handleSubmit}>
+                  Sign Up
+                </Button>
+              </Segment>
+            </Form>
+          </Formik>
+
+          <Message>
+            Already Have An Account? <a href="#">Login</a>
+          </Message>
+        </div>
+      </Grid>
+      
+    </>
   );
 }
