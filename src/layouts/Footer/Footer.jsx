11ab1b7c98cb96278ad6288485deed7cd1d40780
vildntn <vildant35@gmail.com>
2022-01-05 00:44:32 +0300

    Footer updated

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,52 +1,53 @@
 import React from "react";
-import { Menu ,Container} from "semantic-ui-react";
 
 export default function Footer() {
   return (
-      <div>
-        
- <Menu inverted fixed="bottom" >
-     <Container>
-     <div className="ui inverted vertical segment" style={{ padding: 5 }}>
-        <div className="ui container">
-          <div className="ui inverted stackable divided grid">
-            <div className="row">
-              <div className="three wide column">
-                <h4 className="ui inverted header">About</h4>
-                <div className="list" className="ui inverted link list">
-                  <a role="listitem" className="item">
+    <div>
+      <div style={{padding:"8em 0em",backgroundColor:"#691f74"}} class="ui  vertical segment">
+        <div class="ui container">
+          <div class="ui inverted stackable divided grid">
+            <div class="row">
+            <div class="four wide column">
+                <h2 class="ui inverted header footer--title">techorse</h2>
+                <h4>techorse34@gmail.com</h4>
+                <h4>+9958749632545</h4>
+              </div>
+              <div class="three wide column">
+                <h4 class="ui inverted header">About</h4>
+                <div role="list" class="ui inverted link list">
+                  <a role="listitem" class="item">
                     Sitemap
                   </a>
-                  <a role="listitem" className="item">
+                  <a role="listitem" class="item">
                     Contact Us
                   </a>
-                  <a role="listitem" className="item">
+                  <a role="listitem" class="item">
                     Religious Ceremonies
                   </a>
-                  <a role="listitem" className="item">
+                  <a role="listitem" class="item">
                     Gazebo Plans
                   </a>
                 </div>
               </div>
-              <div className="three wide column">
-                <h4 className="ui inverted header">Services</h4>
-                <div role="list" className="ui inverted link list">
-                  <a role="listitem" className="item">
+              <div class="three wide column">
+                <h4 class="ui inverted header">Services</h4>
+                <div role="list" class="ui inverted link list">
+                  <a role="listitem" class="item">
                     Banana Pre-Order
                   </a>
-                  <a role="listitem" className="item">
+                  <a role="listitem" class="item">
                     DNA FAQ
                   </a>
-                  <a role="listitem" className="item">
+                  <a role="listitem" class="item">
                     How To Access
                   </a>
-                  <a role="listitem" className="item">
+                  <a role="listitem" class="item">
                     Favorite X-Men
                   </a>
                 </div>
               </div>
-              <div className="seven wide column">
-                <h4 className="ui inverted header">Footer Header</h4>
+              <div class="six wide column">
+                <h4 class="ui inverted header">Footer Header</h4>
                 <p>
                   Extra space for a call to action inside the footer that could
                   help re-engage users.
@@ -56,10 +57,6 @@ export default function Footer() {
           </div>
         </div>
       </div>
-     </Container>
-     
-    </Menu>
-      </div>
-   
+    </div>
   );
 }
